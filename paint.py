d6deaeb2aafd85592e7101cfcc2ccc8f95b84fef
Espen <espen.myrset@gmail.com>
2020-02-02 19:45:35 +0100

    Variable name changes for tools and widgets

--- paint.py
+++ paint.py
@@ -7,6 +7,8 @@ from PIL import Image as Im, ImageTk, ImageDraw
 from tkinter import *
 
 """ Classes """
+
+
 class paintWindow:
     def __init__(self, fileName):
         self.__root = Tk()
@@ -14,25 +16,26 @@ class paintWindow:
         self.__root.geometry('+0+0')  # Top left position
 
         # Root consists of 3 frames
-        self.__stcToolFrame = Frame(self.__root)  # For static tools
-        self.__stcToolFrame.grid(column=0, row=0, sticky=(W, N, E, S))
-        self.__dynToolFrame = Frame(self.__root)  # For dynamic tools
-        self.__dynToolFrame.grid(column=0, row=1, sticky=(W, N, E, S))
-        self.__imageFrame = Frame(self.__root)  # For image
-        self.__imageFrame.grid(column=1, row=0, rowspan=2, sticky=(W, N, E, S))
+        self.__frameStatic = Frame(self.__root)  # For static tools
+        self.__frameStatic.grid(column=0, row=0, sticky=(W, N, E, S))
+        self.__frameDynamic = Frame(self.__root)  # For dynamic tools
+        self.__frameDynamic.grid(column=0, row=1, sticky=(W, N, E, S))
+        self.__frameImage = Frame(self.__root)  # For image
+        self.__frameImage.grid(column=1, row=0, rowspan=2, sticky=(W, N, E, S))
 
-        # Frames for dynamic frame
-        self.__toolLine = Tool("Line", self.__dynToolFrame)
-        self.__TFreeHand = BooleanVar(value=False)
-        self.__TChkFreeHand = Checkbutton(self.__toolLine.getFrame(), text="Free hand", variable=self.__TFreeHand)
-        self.__TChkFreeHand.grid()
+        # Tools and widgets for dynamic frame
+        self.__toolLine = Tool("Line", self.__frameDynamic)
+        self.__propLineFree = BooleanVar(value=False)
+        self.__widgetLineFree = Checkbutton(self.__toolLine.getFrame(), text="Free hand", variable=self.__propLineFree)
+        self.__widgetLineFree.grid()
 
-        self.__toolRec = Tool("Rectangle", self.__dynToolFrame)
-        self.__TFillBox = BooleanVar(value=False)
-        self.__TChkFreeHand = Checkbutton(self.__toolRec.getFrame(), text="Fill", variable=self.__TFillBox)
-        self.__TChkFreeHand.grid()
+        self.__toolRec = Tool("Rectangle", self.__frameDynamic)
+        self.__propRecFill = BooleanVar(value=False)
+        self.__widgetRecFill = Checkbutton(self.__toolRec.getFrame(), text="Fill", variable=self.__propRecFill)
+        self.__widgetRecFill.grid()
+
         # Grids preset tool
-        self.__toolSel = self.__toolRec.sel()
+        self.__toolSelected = self.__toolRec.select()
 
         # Saves path and filename
         if "/" in fileName:
@@ -42,78 +45,81 @@ class paintWindow:
             self.__path = None
         self.__fileName = fileName
 
-        self.__img = Im.open("{}/{}".format(self.__path, self.__fileName)).convert("RGB")  # main image (PIL image object)
+        self.__img = Im.open("{}/{}".format(self.__path, self.__fileName)).convert(
+            "RGB")  # main image (PIL image object)
         self.__imgPrev = self.__img.copy()  # used for undo
         self.__imgDisplay = self.__img.copy()  # temporary image when drawing (displayed)
         self.__imgDraw = None  # draw reference that changes image object (initialized when moving mouse)
         self.__photoImg = None  # used in canvas (needs to be saved as variable)
 
         self.__imgWdh, self.__imgHgt = self.__img.size
-
-        # Tools (preset values)
-        self.__zoom = None
-        self.resizeToHalfScreen()  # determines suitable zoom ratio
         self.__clickY, self.__clickX = 0, 0
         self.__pressing = False
-        self.__thickness = 5
-        self.__color = (255, 0, 0)
 
         # Button icons
-        self.__TBtnNew = Button(self.__stcToolFrame, text="N", command=None)  # Featured
-        self.__TBtnOpen = Button(self.__stcToolFrame, text="O", command=None)  # Featured
-        self.__TBtnSave = Button(self.__stcToolFrame, text="S", command=self.save)
-        self.__TBtnSaveAs = Button(self.__stcToolFrame, text="SA", command=None)
-        self.__TBtnUndo = Button(self.__stcToolFrame, text="U", command=self.undo)
+        self.__widgetNew = Button(self.__frameStatic, text="N", command=None)  # Featured
+        self.__widgetOpen = Button(self.__frameStatic, text="O", command=None)  # Featured
+        self.__widgetSave = Button(self.__frameStatic, text="S", command=self.save)
+        self.__widgetSaveAs = Button(self.__frameStatic, text="SA", command=None)
+        self.__widgetUndo = Button(self.__frameStatic, text="U", command=self.undo)
         # Tool icons
-        self.__TBtnLine = Button(self.__stcToolFrame, text="L", command=lambda: self.changeTool(self.__toolLine))
-        self.__TBtnRec = Button(self.__stcToolFrame, text="R", command=lambda: self.changeTool(self.__toolRec))
+        self.__widgetLineSelect = Button(self.__frameStatic, text="L", command=lambda: self.changeTool(self.__toolLine))
+        self.__widgetRecSelect = Button(self.__frameStatic, text="R", command=lambda: self.changeTool(self.__toolRec))
+        # Properties (preset values)
+        self.__propThickness = 5
+        self.__propColor = (255, 0, 0)
+        self.__propZoom = None
+        self.resizeToHalfScreen()  # determines suitable zoom ratio
 
-        self.autoGrid(self.__stcToolFrame, 32,
-                      [[self.__TBtnNew, self.__TBtnOpen, self.__TBtnSave, self.__TBtnSaveAs],
-                       [self.__TBtnUndo],
-                       [self.__TBtnLine, self.__TBtnRec]])
+        self.autoGrid(self.__frameStatic, 32,
+                      [[self.__widgetNew, self.__widgetOpen, self.__widgetSave, self.__widgetSaveAs],
+                       [self.__widgetUndo],
+                       [self.__widgetLineSelect, self.__widgetRecSelect]])
 
-        # bd and highlightthickness avoids edge around canvas
-        self.__imgCanvas = Canvas(self.__imageFrame, height=self.__imgHgt*self.__zoom,
-                                  width=self.__imgWdh*self.__zoom, bd=0, highlightthickness=0)
+        # creates Canvas (bd and highlightthickness avoids edge around canvas)
+        self.__imgCanvas = Canvas(self.__frameImage, height=self.__imgHgt * self.__propZoom,
+                                  width=self.__imgWdh * self.__propZoom, bd=0, highlightthickness=0)
         self.__imgCanvas.grid()
         self.updateImage()
 
+        # bind actions
         self.__imgCanvas.bind("<Button-1>", self.mousePressHandler)  # right click
         self.__imgCanvas.bind("<Motion>", self.mouseMoveHandler)
         self.__imgCanvas.bind("<ButtonRelease>", self.mouseReleaseHandler)
-
         self.__root.bind("<Control-z>", self.undo)
 
-        mainloop()
+        self.__root.mainloop()
 
     def mousePressHandler(self, event=None):
         self.__pressing = True
-        self.__clickY = event.y//self.__zoom
-        self.__clickX = event.x//self.__zoom
+        self.__clickY = event.y // self.__propZoom
+        self.__clickX = event.x // self.__propZoom
 
-    def mouseMoveHandler(self, event=None):  # makes and displays a new image whenever mouse is moving
+    # makes and displays a new image whenever mouse is moving
+    def mouseMoveHandler(self, event=None):
         if self.__pressing:
             self.__imgDisplay = self.__img.copy()
             self.__imgDraw = ImageDraw.Draw(self.__imgDisplay)
 
-            eventY = event.y//self.__zoom
-            eventX = event.x//self.__zoom
-            if self.__toolSel == self.__toolLine:
+            eventY = event.y // self.__propZoom
+            eventX = event.x // self.__propZoom
+            if self.__toolSelected == self.__toolLine:
                 self.drawLine(eventY, eventX)
-            elif self.__toolSel == self.__toolRec:
+            elif self.__toolSelected == self.__toolRec:
                 self.drawRectangle(eventY, eventX)
             self.updateImage()
 
-    def mouseReleaseHandler(self, event=None):  # saves changes
+    # saves changes
+    def mouseReleaseHandler(self, event=None):
         self.__pressing = False
         self.__imgPrev = self.__img.copy()
         self.__img = self.__imgDisplay.copy()
 
-    def updateImage(self):  # resize and display sketch image
+    # resize and display sketch image
+    def updateImage(self):
         # PhotoImage used in canvas (resample=0 avoids filter when zooming/resizing)
         self.__photoImg = ImageTk.PhotoImage(self.__imgDisplay.resize((
-            self.__imgWdh*self.__zoom, self.__imgHgt*self.__zoom), resample=0))
+            self.__imgWdh * self.__propZoom, self.__imgHgt * self.__propZoom), resample=0))
         self.__imgCanvas.create_image(0, 0, image=self.__photoImg, anchor=NW)  # insert image in upper left corner
 
     # Grids array with widgets with the same row/column as they are placed in array (calculates columnspan)
@@ -135,7 +141,7 @@ class paintWindow:
             lastRow += 1
 
         for col in range(colMax):  # Insert canvas to make even column spacing and line at end of toolbar
-            Canvas(frame, width=colSize, height=3, bd=0, highlightthickness=0, bg="black")\
+            Canvas(frame, width=colSize, height=3, bd=0, highlightthickness=0, bg="black") \
                 .grid(column=col, row=lastRow)
 
     def save(self):
@@ -147,8 +153,8 @@ class paintWindow:
         self.updateImage()
 
     def changeTool(self, newTool):
-        self.__toolSel.unGrid()
-        self.__toolSel = newTool.sel()
+        self.__toolSelected.unGrid()
+        self.__toolSelected = newTool.select()
 
     def drawLine(self, eventY, eventX):
         startY = self.__clickY
@@ -158,10 +164,10 @@ class paintWindow:
         dY = abs(startY - eventY)
         dX = abs(startX - eventX)
 
-        if not self.__TFreeHand.get():
-            if dY >= 2*dX:  # Line: |
+        if not self.__propLineFree.get():
+            if dY >= 2 * dX:  # Line: |
                 endX = startX
-            elif dX >= 2*dY:  # Line: -
+            elif dX >= 2 * dY:  # Line: -
                 endY = startY
 
             else:  # Line: / or \
@@ -176,24 +182,30 @@ class paintWindow:
                 endY = startY + length
                 endX = startX + length if backslash else startX - length
 
-        self.__imgDraw.line([startX, startY, endX, endY], fill=self.__color, width=self.__thickness)
+        self.__imgDraw.line([startX, startY, endX, endY], fill=self.__propColor, width=self.__propThickness)
 
     def drawRectangle(self, eventY, eventX):
-        if self.__TFillBox.get():
-            fill = self.__color
+        startX = min(self.__clickX, eventX)
+        endX = max(self.__clickX, eventX)
+        startY = min(self.__clickY, eventY)
+        endY = max(self.__clickY, eventY)
+        if self.__propRecFill.get():
+            fill = self.__propColor
         else:
             fill = None
-        self.__imgDraw.rectangle([self.__clickX, self.__clickY, eventX, eventY], fill=fill, outline=self.__color, width=self.__thickness)
+        self.__imgDraw.rectangle([startX, startY, endX, endY], fill=fill, outline=self.__propColor,
+                                 width=self.__propThickness)
 
     def resizeToHalfScreen(self):
         screenwidth = self.__root.winfo_screenwidth()
         screenheight = self.__root.winfo_screenheight()
-        self.__zoom = round(max(1, min((screenheight / self.__imgHgt) // 2, (screenwidth / self.__imgWdh) // 2)))
+        self.__propZoom = round(max(1, min((screenheight / self.__imgHgt) // 2, (screenwidth / self.__imgWdh) // 2)))
+
 
 class Tool:
     def __init__(self, name, parentFrame):
         self.__name = name
-        self.__frame = Frame(parentFrame)
+        self.__frame = Frame(parentFrame)  # used for tools to grid to dynamic frame
 
     def getFrame(self):
         return self.__frame
@@ -204,16 +216,17 @@ class Tool:
     def unGrid(self):
         self.__frame.grid_remove()
 
-    def sel(self):  # grids dynamic frame returns self when tool is selected
+    # grids dynamic frame returns self when tool is selected
+    def select(self):
         self.grid()
         return self
 
     def __str__(self):
         return self.__name
 
+
 """ Functions """
 
-
 """ Loose code """
-paintWindow("images/image1.png")
-
+if __name__ == "__main__":
+    paintWindow("images/image1.png")
